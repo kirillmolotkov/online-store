7d9d8ced0a27beac909f1f45e70cd12a5a914fa6
Maxxxer <polonets.maksim@gmail.com>
2022-12-24 23:34:32 +0300

    feat: add auxillary functions and types

--- src/components/checkout/Cart.ts
+++ src/components/checkout/Cart.ts
@@ -1,8 +1,8 @@
 import { IbasketItem, IproductItem } from '../../types/interfaces';
 
-let CATALOGUE: Map<string, IproductItem>;
+export let CATALOGUE: Map<string, IproductItem>;
 
-async function getCatalogue() {
+export async function getCatalogue() {
   let response = await fetch('/dist/data/data.json');
   let CAT: [IproductItem] = await response.json();
 
@@ -21,51 +21,66 @@ getCatalogue().then((data) => {
 export class Checkout {
   private basket: Map<string, IbasketItem> = new Map();
 
-  addToCart(id: string): boolean {
+  public getBasket(): IbasketItem[] {
+    return [...this.basket.values()];
+  }
+  public loadBasketFromStorage(): void {
+    let cache: string | null = localStorage.getItem('basket')
+    if (cache) {
+      this.basket = new Map(JSON.parse(cache));
+      console.log(this.basket)
+    }
+  }
+  private updateCache(): void{
+   localStorage.setItem('basket', JSON.stringify([...this.basket]))
+  }
+
+  public addToCart(id: string): boolean {
     let prod = CATALOGUE.get(id);
     let itemInBasket: IbasketItem | undefined = this.basket.get(id);
     if (prod) {
       if (itemInBasket) {
         if (prod.stock > itemInBasket.amount) {
           itemInBasket.amount += 1;
-          console.log(this.basket);
+          this.updateCache();
           return true;
         } else return false;
       } else {
-        console.log('hi');
         itemInBasket = { id: prod.id, amount: 1 };
         this.basket.set(id, itemInBasket);
-        console.log(this.basket);
+        this.updateCache();
         return true;
       }
     }
+    
     return false;
   }
 
-  deleteFromCart(id: string): void {
+  private deleteFromCart(id: string): void {
     let itemInBasket: IbasketItem | undefined = this.basket.get(id);
     if (itemInBasket) {
       this.basket.delete(id);
+      this.updateCache();
     }
   }
 
-  decreaseItemAmount(id: string) {
+  public decreaseItemAmount(id: string) {
     let itemInBasket: IbasketItem | undefined = this.basket.get(id);
     if (itemInBasket) {
       if (itemInBasket.amount > 1) {
         itemInBasket.amount -= 1;
-        console.log(this.basket);
         return true;
       } else this.deleteFromCart(id);
     }
+    this.updateCache();
     return false;
   }
 
-  getItemsCount(): number {
-    return [...this.basket.values()].reduce((sum, value) => sum + value.amount, 0)
+  public getItemsCount(): number {
+    return [...this.basket.values()].reduce((sum, value) => sum + value.amount, 0);
   }
 
-  getTotalSum(): number {
+ public getTotalSum(): number {
     let sums: number[] = [];
     this.basket.forEach((value) => {
       let prod = CATALOGUE.get(value.id);
@@ -76,10 +91,9 @@ export class Checkout {
     return sums.reduce((sum, item) => sum + item, 0);
   }
 
-  getItemAmount(id:string): number | undefined {
+ public getItemAmount(id: string): number | undefined {
     return this.basket.get(id)?.amount;
   }
-
 }
 
 export default Checkout;
