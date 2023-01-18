1e154be5dea12f4fb7a37e8d6f8143fccd33a96d
Kirill Molotkov <balbes_goda@mail.ru>
2023-01-18 13:03:04 +0300

    refactor: add new const value and added falsehood check

--- src/components/generate-card/messageNotFoundProduct.ts
+++ src/components/generate-card/messageNotFoundProduct.ts
@@ -3,7 +3,7 @@ import { sectionGoods } from './generateCardItems';
 export const messageNotFoundProduct = (counterFoundProducts: number) => {
   const message = document.createElement('p');
 
-  if (counterFoundProducts === 0) {
+  if (!counterFoundProducts) {
     message.className = 'message-not-found';
     message.innerText = 'Not found products by this filters';
   }
